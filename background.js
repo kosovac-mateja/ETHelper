50f0269ec6a40eee42bb6e04e097a96c892235ef
Bogdan Stojadinovic <mi20073@alas.matf.bg.ac.rs>
2022-12-03 20:08:40 +0100

    log selected text

--- background.js
+++ background.js
@@ -1,14 +1,2 @@
 // kad kliknem na ekstenziju u listi ekstenzija
-
-// window.addEventListener('contextmenu', (ev) => {
-//     function getSelectionText() {
-//         var text = "";
-//         if (window.getSelection) {
-//             text = window.getSelection().toString();
-//         } else if (document.selection && document.selection.type != "Control") {
-//             text = document.selection.createRange().text;
-//         }
-//         console.log(text);
-//         return text;
-//     }
-// });
+alert('background');
--- content.js
+++ content.js
@@ -1,15 +1,14 @@
 // kad se ucita stranica
-// function getSelectionText() {
-//     var text = "";
-//     if (window.getSelection) {
-//         text = window.getSelection().toString();
-//     } else if (document.selection && document.selection.type != "Control") {
-//         text = document.selection.createRange().text;
-//     }
-//     console.log(text);
-//     return text;
-// }
+function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
 
-// document.onmouseup = document.onkeyup = document.onselectionchange = function() {
-//     console.log(getSelectionText());
-//   };
+document.onmouseup = document.onkeyup = document.onselectionchange = function() {
+    console.log(getSelectionText());
+  };
