5c2d2a83ac932591173e340744b8b7cd339dcaff
Bogdan Stojadinovic <mi20073@alas.matf.bg.ac.rs>
2022-12-03 19:07:20 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,2 +1,14 @@
 // kad se ucita stranica
-alert("Hello!");
+(async () => {
+    const results = await Promise.all(urls.map(processUrl));
+    console.log(results);
+})();
+
+async function processUrl(url) {
+    try {
+        const text = await (await fetch(url)).text();
+        return { url, text, status: detecting(text) };
+    } catch (error) {
+        return { url, error };
+    }
+}
